b7fdab9987eafceef184fc02735edd493d0b5110
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-02-08 12:02:12 +0530

    298

--- 19-forms/src/components/Login.jsx
+++ 19-forms/src/components/Login.jsx
@@ -1,6 +1,7 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState();
   const email = useRef();
   const password = useRef();
 
@@ -10,8 +11,11 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword);
-    event.target.reset();
+    const emailIsValid = enteredEmail.includes("@");
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
   }
 
   return (
@@ -21,7 +25,8 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" ref={email} />
+          <input id="email" type="text" name="email" ref={email} />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
         </div>
 
         <div className="control no-margin">
