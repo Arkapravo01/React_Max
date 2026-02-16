a356375d313c947076ef98506451d5193a82be3c
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-02-16 22:45:12 +0530

    322

---
+++ 22-food-order-app/src/store/CartContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    // .... update state to add a meal item
+    const existingCartItemIndex = state.items.findIndex(
+      () => item.id === action.item.id,
+    );
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    // .... update state to remove a meal item
+  }
+
+  return state;
+}
+
+function CartContextPorvider({ children }) {
+  useReducer(cartReducer, { items: [] });
+
+  return <CartContext.Provider>{children}</CartContext.Provider>;
+}
+
+export default CartContext;
