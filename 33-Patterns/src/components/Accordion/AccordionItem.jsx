96b6f27f8cc3658b75b7635ac1054a2ea5a5db92
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-04-08 21:08:12 +0530

    584

---
+++ 33-Patterns/src/components/Accordion/AccordionItem.jsx
@@ -0,0 +1,25 @@
+import { useContext, createContext } from "react";
+
+const AccordionItemContext = createContext();
+
+export function useAccordionItemContext() {
+  const ctx = useContext(AccordionItemContext);
+
+  if (!ctx) {
+    throw new Error(
+      "Accordion Item-related components must be wrapped by <Accordion.Item>"
+    );
+  }
+
+  return ctx;
+}
+
+const AccordionItem = ({ id, className, children }) => {
+  return (
+    <AccordionItemContext.Provider value={{ id }}>
+      <li className={className}>{children}</li>
+    </AccordionItemContext.Provider>
+  );
+};
+
+export default AccordionItem;
