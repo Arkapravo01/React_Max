4cc76014cbda1aeb742875e87eb74e2c117ad682
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-04-09 21:25:18 +0530

    605

---
+++ 35-Testing/src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Greeting = () => {
+  return (
+    <>
+      <h2>Hello World!</h2>
+      <p>It's good to see you</p>
+    </>
+  );
+};
+
+export default Greeting;
