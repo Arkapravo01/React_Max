4776fca63d9d8ad19e37c13f8a08d1f99f7c6be8
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-04-12 16:18:35 +0530

    624

---
+++ 36-Final/src/components/Newpost.jsx
@@ -0,0 +1,24 @@
+import classes from "./NewPost.module.css";
+
+function NewPost() {
+  let enteredBody = "";
+
+  function changeBodyHandler(event) {
+    enteredBody = event.target.value;
+  }
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea onChange={changeBodyHandler} id="body" required rows={3} />
+      </p>
+      <p>{enteredBody}</p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required />
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
